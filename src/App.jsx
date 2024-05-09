5d91af8fe679fd415e645877be09fb183f0ac95c
Tuan Nguyen <dev.nmtuan@gmail.com>
2024-05-09 11:21:51 +0700

    OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
-import React,{useCallback, useEffect,useState }from 'react';
-import ReactFlow,{Background,useEdgesState,Controls,MiniMap,
-  useNodesState,addEdge} from 'reactflow';
+import React,{useCallback, useEffect,useState ,useMemo}from 'react';
+import ReactFlow,{Background,applyNodeChanges,Controls,MiniMap,
+  applyEdgeChanges,addEdge} from 'reactflow';
 import 'reactflow/dist/style.css';
- 
+import { Handle, Position } from 'reactflow';
+import autoprefixer from 'autoprefixer';
+import './index.css';
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '1', type: 'customNode',position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialEdges = [
+  { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'a' },
+  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'b' },
+];
+
  
 export default function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  
   const [processList,setProcessList] = useState([]);
   const [pageIndex, setPageIndex] = useState(0);
+  const [selectedNode, setSelectedNode] = useState(null);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [dropdownPosition, setDropdownPosition] = useState({ x: 0, y: 0 });
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
   useEffect(()=> {
     getAllProcessUser();
+    console.log(processList);
   },[])
+  useEffect(() => {
+      setNodes((nds) =>
+        nds.map((node) => {
+          if (node.type === 'customNode') {
+            return { ...node, data: { ...node.data, processList: processList } };
+          }
+          return node;
+        })
+      );
+      console.log(nodes);
+    }, [processList]);
+
   const getAllProcessUser = () => {
     const queryParams = new URLSearchParams(window.location.search);
     const authId = queryParams.get('authId');
@@ -25,9 +58,7 @@ export default function App() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        body: JSON.stringify({ data: { authId: authId, pageIndex: pageIndex } }),
-      }), // Convert data to JSON string
+      body: JSON.stringify({ data: { authId: authId, pageIndex: pageIndex } }), 
     };
     const PROCESS_LIST_API = `https://auto-web-server-7ko8h.ondigitalocean.app/process/list`;
     fetch(PROCESS_LIST_API, options)
@@ -45,20 +76,42 @@ export default function App() {
         // You can display an error message or take other actions here
       });
   };
-
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
-  
+
+  function CustomNode({ data }) {
+ 
+    return (
+      <div style={{borderWidth: 1}} className='py-2 px-2 bg-white rounded border-black' >
+          <select>
+            {data?.processList?.map((process, index) => (
+              <option key={index} value={process.id}>
+                {process.name}
+              </option>
+            ))}
+          </select>
+      </div>
+    );
+  }
+
+  const nodeTypes = useMemo(() => ({ customNode: CustomNode }), [])
+  const onNodeClick = (event, node) => {
+    setSelectedNode({ node, position: { x: event.clientX, y: event.clientY } });
+    setDropdownPosition({ x: event.clientX, y: event.clientY });
+    setShowDropdown(true);
+  };
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      {/* <Text>{processList}</Text> */}
-      <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect}>
+
+      <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect} onNodeClick={onNodeClick} nodeTypes={nodeTypes}>
       <Controls />
       <MiniMap />
       <Background/>
       </ReactFlow>
+      
     </div>
   );
 }
+
